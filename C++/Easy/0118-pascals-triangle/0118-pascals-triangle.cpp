42aa4736dad5ff247016ed879af9e123e6084ca4
Syed Aafreen <syde967aafn@gmail.com>
2026-07-23 13:25:35 +0530

    0118-pascals-triangle Array Easy .cpp

--- C++/Easy/0118-pascals-triangle/0118-pascals-triangle.cpp
+++ C++/Easy/0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    vector<int>ge(int rows){
-         long long ans = 1;
-        vector<int>ansRow;
-        ansRow.push_back(1);
-        for ( int i = 1 ; i < rows; i++){
-            ans = ans * (rows - i);
-            ans = ans/ (i) ;
-            ansRow.push_back(ans);
+   vector<int>gen(int n){
+    vector<int>ans;
+    long long val = 1;
+    ans.push_back(1);
+    for(int i = 1; i < n ; i++){
+        val = val * (n-i);
+        val = val /(i);
+        ans.push_back(val);
 
-        }
-        return ansRow;
     }
+    return ans;
+    
+   }
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>ans;
-        for ( int i = 1; i <=numRows; i++){
-            ans.push_back(ge(i));
-
+        vector<vector<int>>final;
+        for(int i = 1; i <= numRows; i++){
+            final.push_back(gen(i));
         }
-        return ans;
+        return final;
         
     }
 };
