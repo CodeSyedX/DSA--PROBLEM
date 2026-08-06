85e7890a5741b919070598a1058c042a335de26a
Syed Aafreen <syde967aafn@gmail.com>
2026-08-06 14:55:44 +0530

    0131-palindrome-partitioning String Medium .cpp

--- C++/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ C++/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-bool check(string str){
-    int i = 0;
-    int j = str.size()-1;
-    while(i <=j){
-        if(str[i] != str[j]) return false;
+  bool palin(string s){
+    int i =0;
+    int j = s.size()-1;
+    while(i<=j){
+        if(s[i] != s[j])return false;
         i++;
         j--;
+
     }
     return true;
-}
- void eval(string s , int index, vector<string>& res,  vector<vector<string>>& ans){
-    if (index == s.size()) {
-            ans.push_back(res);
-            return;
+  }
+    void partition(int index , string str, vector<string>& ans, vector<vector<string>>&res){
+        if(index == str.size()){
+            res.push_back(ans);
+            return ;
         }
-          string str = "";
-          for(int i = index ; i < s.size(); i++){
-            str+=s[i];
-            if(check(str) == true){
-                res.push_back(str);           
-                eval(s, i + 1, res, ans);      
-                res.pop_back();
+        string s = "";
+        for(int i = index; i <str.size(); i++){
+            s+=str[i];
+            if(palin(s) == true){
+             ans.push_back(s);
+            partition(i+1,str, ans , res );
+            ans.pop_back();
             }
-          }
-
-          return;
-
-      
-       
+        }
     }
     vector<vector<string>> partition(string s) {
-       vector<vector<string>> ans;
-        vector<string> res;
-
-     
-        eval(s, 0, res, ans);
+        vector<vector<string>>res;
         
-        return ans;
+        vector<string>ans;
+        partition(0,s, ans,res);
+        return res;
+
+
+        
     }
 };
