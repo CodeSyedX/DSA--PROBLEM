679dc8f19d217f734461984144ccd727e0de85d2
Syed Aafreen <syde967aafn@gmail.com>
2026-08-18 15:31:10 +0530

    0547-number-of-provinces Depth-First Search Medium .cpp

--- C++/Medium/0547-number-of-provinces/0547-number-of-provinces.cpp
+++ C++/Medium/0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,32 +1,38 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
-
-   void dfs(int node , vector<int>adj[] , vector<int>&vec){
-    vec[node] = 1;
-    for( auto it : adj[node] ){
-        if(!vec[it]){
-            dfs(it , adj , vec);
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& visited) {
+        visited[node] = 1;
+        for (auto it : adj[node]) {
+            if (!visited[it]) {
+                dfs(it, adj, visited);
+            }
         }
     }
-   }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int>adj[n];
-        for(int i = 0; i < n ;i++){
-            for(int j = 0; j < n ; j++){
-                if(isConnected[i][j] == 1 && i != j){
+        
+        // 1. Build adjacency list
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
                     adj[i].push_back(j);
-            
                 }
             }
         }
-        vector<int>vec(n,0);
+
+        // 2. Traversal to count connected components
+        vector<int> visited(n, 0);
         int cnt = 0;
-        for( int i = 0 ;i < n ; i++){
-            
-            if(!vec[i]){
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 cnt++;
-                dfs(i , adj , vec);
+                dfs(i, adj, visited);
             }
         }
         return cnt;
