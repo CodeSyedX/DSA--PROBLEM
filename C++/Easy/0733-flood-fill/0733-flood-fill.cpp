0a4adac510b3fe99655f9fbb717763d853ccac5b
Syed Aafreen <syde967aafn@gmail.com>
2026-08-18 17:13:54 +0530

    0733-flood-fill Array Easy .cpp

---
+++ C++/Easy/0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void dfs(int row , int col , vector<vector<int>>& ans , vector<vector<int>>&image ,int color,  int delrow[], int delcol[],int incolor ){
+        ans[row][col] = color;
+        int n = image.size();
+        int m = image[0].size();
+        for(int i = 0 ; i < 4; i++){
+            int nrow = row+delrow[i];
+            int ncol = col + delcol[i];
+            if(nrow>= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == incolor && ans[nrow][ncol] != color){
+                dfs(nrow, ncol , ans, image, color, delrow, delcol,incolor );
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n  = image.size();
+        int m = image[0].size();
+        vector<vector<int>>ans = image;
+        int delrow[]= {-1 , 0 , 1, 0};
+        int delcol[] = {0, 1,0, -1};
+        int incolor = image[sr][sc];
+        dfs(sr , sc , ans , image , color ,delrow, delcol, incolor );
+        return ans;
+        
+    }
+};
