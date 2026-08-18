b485928d1173649fef5134488372903ebb6f7e5d
Syed Aafreen <syde967aafn@gmail.com>
2026-08-18 15:30:36 +0530

    0547-number-of-provinces Depth-First Search Medium .cpp

--- C++/Medium/0547-number-of-provinces/0547-number-of-provinces.cpp
+++ C++/Medium/0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-   void dfs(int node ,vector<int> & vis ,vector<int> ad[]  ){
-         vis[node] = 1;
-         for( auto it : ad[node]){
-            if (!vis[it]) {
-            dfs( it , vis , ad);
-            }
-         }
+
+   void dfs(int node , vector<int>adj[] , vector<int>&vec){
+    vec[node] = 1;
+    for( auto it : adj[node] ){
+        if(!vec[it]){
+            dfs(it , adj , vec);
+        }
+    }
    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int>ad[n];
-        for(int i = 0 ; i < n ; i++){
-            for( int j = 0 ; j < n ; j++){
-                if(isConnected[i][j] == 1 && i!= j){
-                    ad[i].push_back(j);
-                   
+        vector<int>adj[n];
+        for(int i = 0; i < n ;i++){
+            for(int j = 0; j < n ; j++){
+                if(isConnected[i][j] == 1 && i != j){
+                    adj[i].push_back(j);
+            
                 }
             }
         }
-        vector<int>vis(n);
+        vector<int>vec(n,0);
         int cnt = 0;
-        for(int i = 0; i < n ; i++){
-            if(!vis[i]){
+        for( int i = 0 ;i < n ; i++){
+            
+            if(!vec[i]){
                 cnt++;
-                dfs(i , vis , ad);
+                dfs(i , adj , vec);
             }
         }
         return cnt;
-
     }
 };
