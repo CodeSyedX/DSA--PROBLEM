acb9ed8f2239abdd6edf196d3a68080dc35e6b24
Syed Aafreen <syde967aafn@gmail.com>
2026-08-22 16:14:10 +0530

    0785-is-graph-bipartite Depth-First Search Medium .cpp

--- C++/Medium/0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ C++/Medium/0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    bool dfs(int node,int col ,vector<int> & vis, vector<vector<int>>&adj){
-        vis[node] = col;
-   for(auto it : adj[node]){
-    if(vis[it] == -1){
-        if(dfs(it, !col ,vis, adj) == false) return false;
-    }
-    else if(vis[it] == col) return false;
 
-   }
-   return true;
-       
+   bool dfs( int i , int col , vector<vector<int>>& graph , vector<int>& vis)
+{
+    vis[i] = col;
+    for( auto it : graph[i]){
+        if(vis[it] == -1){
+            if(dfs(it, !col , graph , vis) == false) return false;
+         
+        }
+   else if(vis[it] == col){
+                return false;
+            }
     }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int v  = graph.size();
-        vector<int> vis(v, -1);
-        for(int i = 0;i<v;i++){
+    return true;
+}    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int>vis(n, -1);
+        for( int i = 0 ; i < n ;i++){
             if(vis[i] == -1){
-                if(dfs(i,0,vis,graph) == false)return false;
+                if(dfs(i , 0 , graph , vis)== false) return false;
             }
         }
         return true;
-
     }
 };
