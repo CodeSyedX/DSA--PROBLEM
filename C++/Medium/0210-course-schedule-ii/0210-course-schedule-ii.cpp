158fc104cbf87f3aa8ac809e136a9499600dee8d
Syed Aafreen <syde967aafn@gmail.com>
2026-08-24 23:47:15 +0530

    0210-course-schedule-ii Depth-First Search Medium .cpp

--- C++/Medium/0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ C++/Medium/0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-         
-        vector<vector<int>> adj(numCourses);
-         for(auto it  : prerequisites){
-            int course = it[0]; // Dependent course
-            int prerequisite = it[1]; // Course that must be taken first
-            adj[prerequisite].push_back(course);
-         }
-
-        vector<int> indegree(numCourses , 0);
-         for( int i = 0; i< numCourses;i++){
-            for( auto it : adj[i]){
-                indegree[it]++;
-            }
-         }
-
-         queue<int>q;
-        for( int i = 0 ; i < numCourses;i++){
-            if(indegree[i] == 0){
+        int n = numCourses;
+       vector<int>adj[n];
+       for(auto it:prerequisites){
+        adj[it[1]].push_back(it[0]);
+       }
+       vector<int>indeg(n);
+       for( int i =0 ; i <n ; i++){
+        for(auto it : adj[i]){
+            indeg[it]++;
+        }
+       }
+       queue<int>q;
+        for(int i = 0;i < n ; i++){
+            if(indeg[i] == 0){
                 q.push(i);
             }
         }
-
-        vector<int>topo;
+      vector<int>topo;
         while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-
-            for( auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
-            }
+          int node = q.front();
+        topo.push_back(node);
+          q.pop();
+          for(auto it : adj[node]){
+            indeg[it]--;
+            if(indeg[it]==0)q.push(it);
+          }
         }
-        if (topo.size() == numCourses) return topo;
+      if (topo.size() == n) {
+            return topo;
+        }
         return {};
-
     }
 };
