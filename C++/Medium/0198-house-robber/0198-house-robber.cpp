3ced95dd6e983817eb4e1e677684b6c7ee89393d
Syed Aafreen <syde967aafn@gmail.com>
2026-08-30 16:39:50 +0530

    0198-house-robber Array Medium .cpp

--- C++/Medium/0198-house-robber/0198-house-robber.cpp
+++ C++/Medium/0198-house-robber/0198-house-robber.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-
-int f(int ind, vector<int> & nums , vector<int> & dp){
-    if(ind == 0 ) return nums[ind];
-    if( ind < 0 ) return 0;
-    if(dp[ind] != -1) return dp[ind];
-    int pick = nums[ind] + f(ind-2,nums ,dp);
-    int not_pick = 0 + f(ind-1, nums , dp);
-    return  dp[ind] = max(pick , not_pick );
-}
-
-    int rob(vector<int>& nums) {
-      
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        if(n < 0 ) return 0 ;
-        if(n == 1) return nums[0];
-        
+    int sol(int ind, vector<int>&nums , vector<int>&dp){
+        if(ind == 0) return nums[ind];
+        if(ind < 0) return 0;
+        if(dp[ind] != -1) return   dp[ind];
+        int pick = nums[ind] + sol(ind-2, nums, dp);
+        int not_pick = sol(ind-1, nums, dp);
+       return  dp[ind] = max(pick, not_pick);
        
-        int prev2 = nums[0];
-        int prev = max(nums[0], nums[1]);
-        for( int i = 2 ; i < n ; i ++){
-             
-              int pick = nums[i] + prev2;
-              int not_pick = 0 + prev;
-              int cur = max(pick, not_pick) ;
-              prev2 = prev;
-              prev = cur;
-        }
-        return prev;
+
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n+1, -1);
+        int ans = sol(n-1 , nums, dp);
+        return ans;
         
     }
 };
