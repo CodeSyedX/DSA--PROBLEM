444ad2552edca49123fff62f0d78176e33526dab
Syed Aafreen <syde967aafn@gmail.com>
2026-08-30 16:47:49 +0530

    0198-house-robber Array Medium .cpp

--- C++/Medium/0198-house-robber/0198-house-robber.cpp
+++ C++/Medium/0198-house-robber/0198-house-robber.cpp
@@ -12,9 +12,19 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1, -1);
-        int ans = sol(n-1 , nums, dp);
-        return ans;
-        
+        vector<int>dp(n+1, 0);
+        dp[0] = nums[0];
+        for(int ind = 1; ind < n ;ind++){
+            
+           int pick = nums[ind];
+           if(ind -2 >= 0){
+             pick+= dp[ind-2];
+           } 
+
+             int not_pick = dp[ind-1];
+             dp[ind]  = max(pick , not_pick);
+
+        }
+        return dp[n-1];
     }
 };
