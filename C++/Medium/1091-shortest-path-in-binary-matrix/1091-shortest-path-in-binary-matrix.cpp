26e8edf1d3f95baa1dd3d3e42311c5050c828e68
Syed Aafreen <syde967aafn@gmail.com>
2026-09-01 16:34:48 +0530

    1091-shortest-path-in-binary-matrix Array Medium .cpp

--- C++/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ C++/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,43 +3,38 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
+        if (grid[0][0] != 0 || grid[n - 1][m - 1] != 0) {
             return -1;
         }
 
-        if (n == 1) {
+        // Special case: 1x1 matrix with clear cell
+        if (n == 1 && m == 1) {
             return 1;
         }
-        queue<pair< int , pair < int , int >>> q;
-        vector < vector < int >> dist(n , vector<int> (m, 1e9 ));
-        dist[0][0] = 1;
-        q.push({1,{0,0}});
-        int dx[] = {0, 0, 1, -1, 1, 1, -1, -1};
-        int dy[] = {1, -1, 0, 0, 1, -1, 1, -1};
+    vector<vector<int>> dis(n, vector<int>(m, 1e9));
+        queue<pair<int , pair<int , int>>>q;
+        q.push({1, {0 , 0}});
+        dis[0][0] = 1;
+       
         while(!q.empty()){
             auto it = q.front();
             q.pop();
-           int dis = it.first;
-            int r = it.second.first;
-            int c = it.second.second;
-            for( int i = 0 ;i < 8 ; i++){
-                int row = r+dx[i];
-                int col = c+ dy[i];
-                if(row >= 0 && row < n && col >= 0  && col < m && grid[row][col] == 0 && dis + 1 < dist[row][col]){
-                    dist[row][col] = dis + 1;
-                    if(row == n-1 && col == m-1) return  dis + 1;
-                    q.push({dis+1 , {row, col}});
+            int wt = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+           for( int i = -1 ; i <= 1; i++){
+            for(int j = -1 ; j <= 1; j++){
+                      int nrow = row + i;
+                int ncol = col + j;
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 0 && wt +1 < dis[nrow][ncol]){
+                    if(nrow == n-1 && ncol == m-1) return wt + 1;
+                    dis[nrow][ncol] = wt + 1;
+                    q.push({dis[nrow][ncol], {nrow, ncol}});
                 }
+           
             }
-        }
-
-    return -1;
-     
-
-
-
-
-
-        
+           }
+        }      
+        return -1;
     }
 };
