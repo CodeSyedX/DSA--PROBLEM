32c65a009ef54bcc72f68481f84750c21bd31a13
Syed Aafreen <syde967aafn@gmail.com>
2026-09-03 15:52:03 +0530

    0062-unique-paths Math Medium .cpp

---
+++ C++/Medium/0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+   int path(int row , int col , vector<vector<int>>&dp){
+    if(row == 0 && col == 0){
+        return 1;
+    }
+    if(row <0 || col < 0){
+        return 0;
+    }
+    if(dp[row][col] != -1) return dp[row][col];
+    int up = path(row -1 , col ,dp);
+    int left = path(row , col -1 , dp);
+      return dp[row][col] = up + left;
+   }
+    int uniquePaths(int m, int n) {
+        if(m ==1 && n==1) return 1;
+      vector<vector<int>>dp(m , vector<int>(n , -1));
+        path(m-1 , n-1 , dp);
+      return dp[m-1][n-1];   
+    }
+};
