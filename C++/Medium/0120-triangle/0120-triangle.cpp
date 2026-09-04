7ee24e6b9ac7284466ac85cab2d2abd4397a98e8
Syed Aafreen <syde967aafn@gmail.com>
2026-09-04 15:47:23 +0530

    0120-triangle Array Medium .cpp

--- C++/Medium/0120-triangle/0120-triangle.cpp
+++ C++/Medium/0120-triangle/0120-triangle.cpp
@@ -15,8 +15,16 @@ public:
         int n = triangle.size();
         if(n == 1 ) return triangle[0][0];
         vector<vector<int>>dp(n , vector<int>(n , 1e9));
-        solve(0 ,0, triangle , dp);
+        for( int i = 0 ; i < n ; i++){
+            dp[n-1][i] = triangle[n-1][i];
+        }
+        for(int i = n-2 ; i >= 0 ; i--){
+            for(int j = i ; j >= 0; j--){
+                int d = triangle[i][j] + dp[i+1][j];
+                int dg = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(d, dg);
+            }
+        }
         return dp[0][0];
-        
     }
 };
