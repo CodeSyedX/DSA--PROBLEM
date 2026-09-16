0d06a6168824d9f2eea60763a0eb01117f389c3c
Syed Aafreen <syde967aafn@gmail.com>
2026-09-16 11:56:38 +0530

    0144-binary-tree-preorder-traversal Stack Easy .cpp

--- C++/Easy/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ C++/Easy/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,27 +11,23 @@
  */
 class Solution {
 public:
- 
-
-  
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(root == NULL)return ans;
+        vector<int>pre;
+        if(root == NULL) return pre;
         stack<TreeNode*>st;
-       st.push(root);
-       while(!st.empty()){
-        TreeNode * node = st.top();
-        st.pop();
-         ans.push_back(node->val);
-         if(node->right != NULL){
-            st.push(node->right);
-         }
-         if(node->left != NULL){
-            st.push(node->left);
-         }
-       }
-       return ans;
-
-    }    
+        st.push(root);
+        while(!st.empty()){
+            TreeNode*node = st.top();
     
+            st.pop();
+             pre.push_back(node->val);
+            if(node->right!=NULL){
+                st.push(node->right);
+            }
+            if(node->left !=NULL){
+                st.push(node->left);
+            }
+        }
+        return pre;
+    }
 };
