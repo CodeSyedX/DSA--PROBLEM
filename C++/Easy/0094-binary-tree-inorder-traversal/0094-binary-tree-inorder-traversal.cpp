bc4bcca0597eb94a1f72d5dc031d7e6a0412cd69
Syed Aafreen <syde967aafn@gmail.com>
2026-09-16 12:20:45 +0530

    0094-binary-tree-inorder-traversal Stack Easy .cpp

--- C++/Easy/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ C++/Easy/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,35 +12,24 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-  
-      vector<int>ans;
-      TreeNode * cur = root ;
-      while(cur != NULL){
-        if(cur->left == NULL){
-             ans.push_back(cur->val);
-             cur = cur->right;
-        }
-        else{
-            
-                TreeNode* prev = cur->left;
-                while(prev->right && prev->right != cur){
-                    prev = prev->right;
-                }
-                if(prev->right == NULL){
-                    prev->right = cur;
-                    cur= cur->left;
-                }
-                else{
-                    prev->right =   NULL;
-                    ans.push_back(cur->val);
-                    cur= cur->right;
-                }
+        vector<int>post;
+        stack<TreeNode*>st;
+        TreeNode*node = root;
+        while(true){
+          
+            if(node!=NULL){
+                  st.push(node); 
+                node = node->left;
             }
-        }
-      
-      return ans;
-
-      
-
-    }  
+            else{
+                if(st.empty()== true) break;
+                    node = st.top();
+                    st.pop();
+                    post.push_back(node->val);
+                    node = node->right;
+                
+            }
+        } 
+        return post;
+    }
 };
