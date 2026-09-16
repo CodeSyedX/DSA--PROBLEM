458c2e561e03a6d8708bd58cc0100d7305ac18ac
Syed Aafreen <syde967aafn@gmail.com>
2026-09-16 12:28:58 +0530

    0104-maximum-depth-of-binary-tree Tree Easy .cpp

--- C++/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ C++/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -13,9 +13,9 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(root == nullptr) return 0;
-        int lh =  maxDepth(root->left);
+        int lh = maxDepth(root->left);
         int rh = maxDepth(root->right);
-        return 1+ max(lh,rh);
-        
+        return 1 + max(lh,rh);
+
     }
 };
